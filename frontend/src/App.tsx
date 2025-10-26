cd63ca95f7670e6ba86564d5831d0722a6eff571
kuslhhh <kushaljadhav7745@gmail.com>
2025-10-27 00:25:49 +0530

    pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
+import MainPage from "./pages/main"
+import SignIn from "./pages/sign-in"
+import RoomPage from "./pages/room"
 
 function App() {
 
-  return (
-   <>
-      <div>
-         hello
-      </div>
-   </>
-    )
+   return (
+      <>
+         <div className=" bg-[#181818] text-[#808080] text-3xl min-h-screen justify-center items-center flex">
+            <Router>
+               <Routes>
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="/signin" element={<SignIn />} />
+                  <Route path="/room" element={<RoomPage />} />
+               </Routes>
+            </Router>
+         </div>
+      </>
+   )
 }
 
 export default App
